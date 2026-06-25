1c6e7734272cf72670cab0b28eaf6cf8cd050dd8
Your Name <yayehasres1221@gmail.com>
2026-06-25 22:19:19 +0300

    updated

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,20 +1,7 @@
-const allowedOrigins = [
-  "http://localhost:5173",
-  //   "https://your-frontend-domain.com",
-];
-
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-  optionsSuccessStatus: 200,
 };
 
 module.exports = corsOptions;
